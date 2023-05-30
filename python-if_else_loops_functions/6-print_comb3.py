84abe14eacd8f9023963567174e400984c61be5e
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-05-30 09:33:00 -0700

    task6, using format()

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for decenas in range(0, 10):
+for decenas in range(0, 9):
     for unidades in range(decenas+1, 10):
         if decenas != 8:
-            print(f"{decenas}{unidades}", end=', ')
+            print("{}{}".format(decenas, unidades), end=', ')
         else:
-            print(f"{decenas}{unidades}")
+            print("{}{}".format(decenas, unidades))
