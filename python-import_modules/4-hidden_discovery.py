af7282545c43caa54dad471990dc9746956ad6fc
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-06-01 07:58:10 -0700

    task4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import hidden_4
 
     names = dir(hidden_4)
-    for i in range(names):
+    for i in names:
         if i[0:2] != "__":
             print("{}".format(i))
