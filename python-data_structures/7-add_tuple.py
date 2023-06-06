39de82b8399ef6b393578e7fbd125226227180ee
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-06-06 07:23:24 -0700

    task7 made

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a[:2] + (0, 0)
+    b = tuple_b[:2] + (0, 0)
+    c = a[0] + b[0]
+    d = a[1] + b[1]
+    new_tuple = (c, d)
+    return (new_tuple)
