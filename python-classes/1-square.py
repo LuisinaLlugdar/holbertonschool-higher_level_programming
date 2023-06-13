7015c37b7833296f565f7fa9b9032490d9e6cc3c
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-06-13 05:56:43 -0700

    task1 made

---
+++ python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+1-square.py:
+Class Square that defines a square
+"""
+
+
+class Square:
+    """
+    Create an object of class Square
+    """
+    def __init__(self, size):
+        """
+        Initialize square
+
+        Attributes:
+            size: private attribute size of square
+        """
+        self.__size = size
