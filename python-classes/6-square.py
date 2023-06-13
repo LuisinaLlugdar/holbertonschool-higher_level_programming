119dd252c0bbcc254fb47671da4e81eed747eaca
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-06-13 08:38:47 -0700

    task6 made

---
+++ python-classes/6-square.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+"""
+5-square.py:
+Class Square that defines a square
+"""
+
+
+class Square:
+    """
+    Create an object of class Square
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Initialize square
+
+        Attributes:
+            size: attribute size of square
+            position: blankspaces before square
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """
+        Getter
+
+        Return:
+            size of square
+        """
+        return self.__size
+
+    @property
+    def position(self):
+        """
+        Getter
+
+        Return:
+            position to start printing square
+        """
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter
+
+        Attributes:
+            value: the new size
+        """
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    @position.setter
+    def position(self, value):
+        """
+        Setter
+
+        Attributes:
+            value: the new position
+        """
+        self.__position = value
+        if type(value) is not tuple or len(value) != 2 \
+           or type(value[0]) is not int or type(value[1]) is not int \
+           or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        """
+        Public instance method that calculates the square area
+
+        Return:
+            area of the current square
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """
+        Prints a square made of '#'
+        with spaces given by position
+        """
+        if self.size == 0:
+            print()
+        else:
+            for row in range(self.size):
+                for lines in range(self.position[0]):
+                    print(" ", end='')
+                for column in range(self.size):
+                    print("#", end='')
+                print()
