73c7d08043ee54232b97daaac90ebadfc5bae0a8
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-06-13 07:59:31 -0700

    task5 made

---
+++ python-classes/5-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+5-square.py:
+Class Square that defines a square
+"""
+
+
+class Square:
+    """
+    Create an object of class Square
+    """
+    def __init__(self, size=0):
+        """
+        Initialize square
+
+        Attributes:
+            size: attribute size of square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Getter
+
+        Return:
+            size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter
+
+        Attributes:
+            value: the new size
+        """
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        Public instance method that calculates the square area
+
+        Return:
+            area of the current square
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """
+        Prints a square made of '#'
+        """
+        if self.size == 0:
+            print()
+        else:
+            for row in range(self.size):
+                for column in range(self.size):
+                    print("#", end='')
+                print()
