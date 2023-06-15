62c7f952d2bde90ea73967324ee04863ee0782eb
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-06-15 13:07:12 -0700

    task4 made, code and test files

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module 4-print_square
+"""
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float):
+        if size < 0:
+            raise TypeError("size must be an integer")
+    
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        print()
