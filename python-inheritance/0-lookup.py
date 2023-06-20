bff08a911f071a25d84a016042043b1f983716ff
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-06-20 07:50:12 -0700

    task0 made

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module 0-lookup
+"""
+
+
+def lookup(obj):
+    """
+    Function that returns list of available attributes and methods of an object
+    """
+    return dir(obj)
