97f186a1f4bc469e6e34662770c6d1a3b5727bff
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-06-22 08:22:20 -0700

    task6 made

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+based on 5-base_geometry
+"""
+
+
+class BaseGeometry:
+    """
+    Public method:
+        area
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
