6814cab4068c744ec0910ca2930d306aeff6a8c3
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-06-27 09:50:24 -0700

    first testing

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Unittest for Rectangle Class
+"""
+import unittest
+from models.rectangle import Rectangle
+
+class TestRectangleClass(unittest.TestCase):
+    """
+    unittests for functions in Rectangle Class
+    """
+
+    def test_empty(self):
