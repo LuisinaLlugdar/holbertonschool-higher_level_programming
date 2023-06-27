81fcb9e0a96f83257c9a7fa5f28e1142b09037dd
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-06-27 09:18:46 -0700

    task2, FirstRectangle, made

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+"""
+Module 2-Rectangle
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    Class Rectangle that inherit from Base Class
+
+    Private Attributes:
+        width
+        height
+        x
+        y
+    Each one of these have their own getter and setter
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Constructor for an instance of the class Rectangle
+
+        Args:
+            width
+            height
+            x
+            y
+            id - comes from the Super Class Base
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+        @property
+        def width(self):
+            """
+            Getter for width
+            """
+            return self.__width
+
+        @property
+        def height(self):
+            """
+            Getter for height
+            """
+            return self.__height
+
+        @property
+        def x(self):
+            """
+            Getter for x
+            """
+            return self.__x
+
+        @property
+        def y(self):
+            """
+            Getter for y
+            """
+            return self.__y
+
+        @width.setter
+        def width(self, width):
+            """
+            Setter for width
+            """
+            self.__width = width
+
+        @height.setter
+        def height(self, height):
+            """
+            Setter for height
+            """
+            self.__height = height
+
+        @x.setter
+        def x(self, x):
+            """
+            Setter for x
+            """
+            self.__x = x
+
+        @y.setter
+        def y(self, y):
+            """
+            Setter for y
+            """
+            self.__y = y
