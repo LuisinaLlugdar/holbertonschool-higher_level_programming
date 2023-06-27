94ca6bd2d208ea54574c38e02a98b986a8a07988
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-06-27 15:11:33 -0700

    task7, Display1, made

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -139,7 +139,11 @@ class Rectangle(Base):
         """
         method that prints in stdout the rectangle with the character #
         """
+        for a in range(self.y):
+            print()
         for i in range(self.__height):
+            for b in range(self.x):
+                print(" ", end='')
             for j in range(self.__width):
                 print("#", end='')
             print()
