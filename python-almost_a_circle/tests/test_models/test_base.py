462da0401a4a7d71204e0b2fad8da5c3e8b7ccb9
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-06-29 12:11:26 -0700

    added testing for representing a dict using json

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -23,6 +23,17 @@ class TestBaseClass(unittest.TestCase):
         b3 = Base(None)
         self.assertEqual(b3.id, 1)
 
+    def test_to_json_string(self):
+        list_dictionaries = [{'i': 1, 'j': 2}, {'x': 3, 'y': 4}]
+        self.assertEqual(Base.to_json_string(list_dictionaries),
+                         '[{"i": 1, "j": 2}, {"x": 3, "y": 4}]')
+
+        list_dictionaries = []
+        self.assertEqual(Base.to_json_string(list_dictionaries), "[]")
+
+        list_dictionaries = None
+        self.assertEqual(Base.to_json_string(list_dictionaries), "[]")
+
 
 if __name__ == '__main__':
     unittest.main()
