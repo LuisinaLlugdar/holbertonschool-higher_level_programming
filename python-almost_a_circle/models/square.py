05f36e58dad0836f6d25fbd78511b69652911534
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-06-29 07:40:22 -0700

    task10, class Square, made

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module 3-Square
+"""
+
+from models.rectangle import Rectangle
+"""
+Importing Rectangle Class to be inherited
+"""
+
+
+class Square(Rectangle):
+    """
+    Class Square that inherit from Rectangle Class
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Constructor for an instance of the class Square
+
+        Args:
+            size (for both width and height)
+            x
+            y
+            id
+        they must me inherited from Rectangle
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        overriding the __str__ method so that it returns a specific message
+        """
+        msg = "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                self.width)
+        return msg
