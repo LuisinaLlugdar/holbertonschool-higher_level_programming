fdf07e2b060388c6f56b10d7cefc0f7c4baaece0
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-06-29 11:52:50 -0700

    task15, dict to json, made

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 Module 1-Base_Class
 """
+import json
 
 
 class Base:
@@ -29,3 +30,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        static method that returns the JSON string representation
+        of list_dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
