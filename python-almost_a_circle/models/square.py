208abd098a2640103deff145d1c8ec305a879a2b
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-06-29 08:29:55 -0700

    trying to fix two checker error

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -64,12 +64,12 @@ class Square(Rectangle):
         if args:
             if len(args) >= 1:
                 self.id = args[0]
-            if len(args) == 2:
-                self.size = args[1]
-            if len(args) == 3:
-                self.x = args[2]
-            if len(args) == 4:
-                self.y = args[3]
+                if len(args) >= 2:
+                    self.size = args[1]
+                if len(args) >= 3:
+                    self.x = args[2]
+                if len(args) >= 4:
+                    self.y = args[3]
         else:
             for k, v in kwargs.items():
                 if k == "id":
